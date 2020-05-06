ec32d568929f158dfc2a060fe75c27cf30777409
Bwoodruff4 <blakewoodruff4@gmail.com>
2020-05-06 14:01:35 -0600

    Added redirect to user page after user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,10 @@ class UsersController < ApplicationController
     def create
         @user = User.create(
             user_name: params[:user_name],
-            zip_code: params[:zip_code]
+            zip_code: params[:zip_code],
+            password: params[:password]
         )
-        render json: @user
+        redirect_to "http://localhost:3001/user.html?id=#{@user.id}"
     end
 
     def update
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_153454) do
+ActiveRecord::Schema.define(version: 2020_05_06_154658) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.string "weather_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string "key"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
