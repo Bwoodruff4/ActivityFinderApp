daad2bca9f336f339864e493a50212de99e9b771
Bwoodruff4 <blakewoodruff4@gmail.com>
2020-05-06 18:44:34 -0600

    Added picture attribute to activity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_154658) do
+ActiveRecord::Schema.define(version: 2020_05_07_004247) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_154658) do
     t.string "weather_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture"
   end
 
   create_table "api_keys", force: :cascade do |t|
